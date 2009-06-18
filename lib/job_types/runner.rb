0ac9ad6651a6e0aec6579f60835ce536de13ab2f
Travis Petticrew <bobo@petticrew.net>
2009-06-18 14:57:20 -0500

    Added :file option to Job::Runner to allow calling runner for a file rather than a ruby snippet.

--- lib/job_types/runner.rb
+++ lib/job_types/runner.rb
@@ -1,10 +1,15 @@
 module Whenever
   module Job
     class Runner < Whenever::Job::Default
-
+      
+      def initialize(options = {})
+        super(options)
+        @file = !!options[:file]
+      end
+      
       def output
         path_required
-        %Q(#{File.join(@path, 'script', 'runner')} -e #{@environment} "#{task}")
+        %Q(#{File.join(@path, 'script', 'runner')} -e #{@environment} #{@file ? task : %Q("#{task}") })
       end
       
     end
